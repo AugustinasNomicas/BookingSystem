9347e3307e11665f48490c0eaa1d327187d63858
Augustinas Nomicas <augustinas.nomicas@gmail.com>
2015-10-31 09:03:02 +0200

    Files clean up

--- Tourtlee.BookingSystem/Tourtlee.BookingSystem.Web/Areas/Admin/Controllers/HomeController.cs
+++ Tourtlee.BookingSystem/Tourtlee.BookingSystem.Web/Areas/Admin/Controllers/HomeController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Authorization;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Tourtlee.BookingSystem.Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Policy = "AccessAdminArea")]
     public class HomeController : Controller
     {
         // GET: /<controller>/
